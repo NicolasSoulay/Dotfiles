e4627698a903544a42a6198c5c014d9691a1aa3a
NicolasSoulay <soulaynicolas@gmail.com>
2024-03-08 17:32:37 +0100

    nvim => numberline false zen mode

--- config/nvim/lua/plugins/zen-mode.lua
+++ config/nvim/lua/plugins/zen-mode.lua
@@ -13,7 +13,7 @@ local M = {
             -- uncomment any of the options below, or add other vim.wo options you want to apply
             options = {
                 -- signcolumn = "no", -- disable signcolumn
-                -- number = false, -- disable number column
+                number = false, -- disable number column
                 -- relativenumber = false, -- disable relative numbers
                 -- cursorline = false, -- disable cursorline
                 -- cursorcolumn = false, -- disable cursor column
