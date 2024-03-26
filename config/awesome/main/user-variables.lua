4326cecf21e1d23dea8652c71ce96da7ca9e96b3
NicolasSoulay <soulaynicolas@gmail.com>
2024-03-26 14:41:46 +0100

    modif awesome

--- config/awesome/main/user-variables.lua
+++ config/awesome/main/user-variables.lua
@@ -1,23 +1,9 @@
--- {{{ Global Variable Definitions
--- moved here in module as local variable
--- }}}
-
-local home = os.getenv("HOME")
-
 local _M = {
-    -- This is used later as the default terminal and editor to run.
-    -- terminal = "xterm",
     terminal = "kitty",
-
-    -- Default modkey.
-    -- Usually, Mod4 is the key with a logo between Control and Alt.
-    -- If you do not like this or do not have such a key,
-    -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
-    -- However, you can use another modifier like Mod1, but it may interact with others.
     modkey = "Mod4",
-    file_manager = "thunar",
-    browser = "firefox"
-
+    file_manager = "mc",
+    browser = "firefox",
+    mail_client = "thunderbird"
 }
 
 return _M
