d37a10a3af47f78b606c57c01a207e3e0003835d
NicolasSoulay <soulaynicolas@gmail.com>
2024-05-22 15:50:34 +0200

    theme awesome

--- config/awesome/README.md
+++ config/awesome/README.md
@@ -1,1 +1,15 @@
 # awesome
+
+## todo
+
+- ~~titlebar buttons focus/unfocused and active/inactive~~
+- layout color
+- volume control
+- wifi manager
+- bluetooth manager
+- update manager
+
+- custom icons for app
+- screenshot tool
+
+- define starting state for app (floating, fullscreen, tilled...)
--- config/awesome/theme.lua
+++ config/awesome/theme.lua
@@ -7,15 +7,14 @@ local xresources                                = require("beautiful.xresources"
 local dpi                                       = xresources.apply_dpi
 
 local gfs                                       = require("gears.filesystem")
-local themes_path                               = gfs.get_themes_dir()
-local assets_path                               = "~/.config/awesome/assets/"
+local themes_path                               = "~/.config/awesome/themes/"
 
 local theme                                     = {}
 
 theme.font                                      = "JetBrainsMono NFM"
 
 theme.bg_normal                                 = "#282828"
-theme.bg_focus                                  = "#353535"
+theme.bg_focus                                  = "#282828"
 theme.bg_urgent                                 = "#ff0000"
 theme.bg_minimize                               = "#444444"
 theme.bg_systray                                = theme.bg_normal
@@ -72,8 +71,8 @@ theme.menu_width                                = dpi(250)
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal              = assets_path .. "close_normal.png"
-theme.titlebar_close_button_focus               = assets_path .. "close_normal.png"
+theme.titlebar_close_button_normal              = themes_path .. "default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus               = themes_path .. "default/titlebar/close_focus.png"
 
 theme.titlebar_minimize_button_normal           = themes_path .. "default/titlebar/minimize_normal.png"
 theme.titlebar_minimize_button_focus            = themes_path .. "default/titlebar/minimize_focus.png"
@@ -88,15 +87,15 @@ theme.titlebar_sticky_button_focus_inactive     = themes_path .. "default/titleb
 theme.titlebar_sticky_button_normal_active      = themes_path .. "default/titlebar/sticky_normal_active.png"
 theme.titlebar_sticky_button_focus_active       = themes_path .. "default/titlebar/sticky_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive  = assets_path .. "floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive   = assets_path .. "floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active    = assets_path .. "floating_normal_active.png"
-theme.titlebar_floating_button_focus_active     = assets_path .. "floating_focus_active.png"
+theme.titlebar_floating_button_normal_inactive  = themes_path .. "default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive   = themes_path .. "default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active    = themes_path .. "default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active     = themes_path .. "default/titlebar/floating_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = assets_path .. "maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = assets_path .. "maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active   = assets_path .. "maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active    = assets_path .. "maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = themes_path .. "default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = themes_path .. "default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active   = themes_path .. "default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active    = themes_path .. "default/titlebar/maximized_focus_active.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh                              = themes_path .. "default/layouts/fairhw.png"
@@ -117,7 +116,7 @@ theme.layout_cornersw                           = themes_path .. "default/layout
 theme.layout_cornerse                           = themes_path .. "default/layouts/cornersew.png"
 
 -- Generate Awesome icon:
-theme.awesome_icon                              = assets_path .. "debian.png"
+theme.awesome_icon                              = themes_path .. "default/debian.png"
 
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
