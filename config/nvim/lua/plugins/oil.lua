42d544fa26fa4173eb58ed3f31aa46637d59bfe8
NicolasSoulay <soulaynicolas@gmail.com>
2024-07-24 22:37:11 +0200

    test oil, added colorizer

---
+++ config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+	"stevearc/oil.nvim",
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	config = function()
+		local oil = require("oil")
+
+		oil.setup({})
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
+}
