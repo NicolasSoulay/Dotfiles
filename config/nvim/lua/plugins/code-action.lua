05525989f930a60c38507952c0515ecf1a746a77
NicolasSoulay <soulaynicolas@gmail.com>
2024-07-25 12:53:40 +0200

    div

--- config/nvim/lua/plugins/code-action.lua
+++ config/nvim/lua/plugins/code-action.lua
@@ -1,14 +1,6 @@
 return {
-	"rachartier/tiny-code-action.nvim",
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-telescope/telescope.nvim" },
-	},
-	event = "LspAttach",
+	"aznhe21/actions-preview.nvim",
 	config = function()
-		require("tiny-code-action").setup()
-		vim.keymap.set("n", "<leader>ca", function()
-			require("tiny-code-action").code_action()
-		end, { noremap = true, silent = true })
+		vim.keymap.set({ "v", "n" }, "<leader>ca", require("actions-preview").code_actions)
 	end,
 }
--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -3,10 +3,34 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
+		local colors = {
+			fg1 = "#282828",
+			color2 = "#32302f",
+			fg2 = "#d4be98",
+			color3 = "#32302f",
+			color4 = "#7daea3",
+			color5 = "#89b482",
+			color6 = "#a9b665",
+			color7 = "#d8a657",
+			color8 = "#a89984",
+			color9 = "#ea6962",
+		}
 		lualine.setup({
 			options = {
 				icons_enabled = true,
-				theme = "auto",
+				theme = {
+					normal = {
+						a = { fg = colors.fg1, bg = colors.color4, gui = "bold" },
+						b = { fg = colors.fg2, bg = colors.color2 },
+						c = { fg = colors.fg2, bg = colors.fg1 },
+					},
+					command = { a = { fg = colors.fg1, bg = colors.color5, gui = "bold" } },
+					inactive = { a = { fg = colors.fg2, bg = colors.color2 } },
+					insert = { a = { fg = colors.fg1, bg = colors.color6, gui = "bold" } },
+					replace = { a = { fg = colors.fg1, bg = colors.color7, gui = "bold" } },
+					terminal = { a = { fg = colors.fg1, bg = colors.color8, gui = "bold" } },
+					visual = { a = { fg = colors.fg1, bg = colors.color9, gui = "bold" } },
+				},
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
