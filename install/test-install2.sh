59bf48bc1572cc0c4ea50c31345bb96e4482b0fb
NicolasSoulay <soulaynicolas@gmail.com>
2024-11-24 23:23:59 +0100

    modifs

--- install/test-install2.sh
+++ install/test-install2.sh
@@ -213,7 +213,7 @@ curl -sS https://starship.rs/install.sh | sh -s -- -y
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo apt update -y
-sudo apt install wezterm -y
+sudo apt install wezterm-nightly -y
 
 # Update final au cas ou, on remove des dependances obsoletes, on remove les fichier qui ne servent plus
 sudo rm ~/.wget-hsts
