df4d84150def8164279659af9dd49c1343caf6ec
NicolasSoulay <soulaynicolas@gmail.com>
2025-01-16 16:23:08 +0100

    [nvim] better diagnostic plugin

--- config/nvim/lua/core/autocomands.lua
+++ config/nvim/lua/core/autocomands.lua
@@ -1,3 +1,4 @@
+-- Use q to close in specifics buffers
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = {
 		"netrw",
@@ -22,6 +23,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
+-- briefly highlight yanked text
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	callback = function()
 		vim.highlight.on_yank({ higroup = "Visual", timeout = 100 })
@@ -40,34 +42,4 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
 			vim.cmd([[silent! lua require("luasnip").unlink_current()]])
 		end
 	end,
-})
-
--- Function to check if a floating dialog exists and if not
--- then check for diagnostics under the cursor
-function OpenDiagnosticIfNoFloat()
-	for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-		if vim.api.nvim_win_get_config(winid).zindex then
-			return
-		end
-	end
-	-- THIS IS FOR BUILTIN LSP
-	vim.diagnostic.open_float(0, {
-		scope = "cursor",
-		focusable = false,
-		close_events = {
-			"CursorMoved",
-			"CursorMovedI",
-			"BufHidden",
-			"InsertCharPre",
-			"WinLeave",
-		},
-	})
-end
-
--- Show diagnostics under the cursor when holding position
-vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
-vim.api.nvim_create_autocmd({ "CursorHold" }, {
-	pattern = "*",
-	command = "lua OpenDiagnosticIfNoFloat()",
-	group = "lsp_diagnostics_hold",
 })
