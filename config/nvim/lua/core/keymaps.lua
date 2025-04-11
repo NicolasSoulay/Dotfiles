7601cf79d73dc87b1d848ef937acc52f13c3d1f6
NicolasSoulay <soulaynicolas@gmail.com>
2025-04-11 15:42:34 +0200

    diver

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -23,9 +23,6 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- Close buffers
-keymap("n", "<S-q>", ":bdelete<CR>", opts)
-
 -- Close window
 keymap("n", "<Leader>q", "<C-w>q", opts)
 
