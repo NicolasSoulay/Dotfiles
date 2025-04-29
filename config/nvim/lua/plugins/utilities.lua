fb5fc05e8c2a33e8e9735a29bb5de65b6ff56295
NicolasSoulay <soulaynicolas@gmail.com>
2025-04-29 15:57:41 +0200

    nvim diag

--- config/nvim/lua/plugins/utilities.lua
+++ config/nvim/lua/plugins/utilities.lua
@@ -12,11 +12,14 @@ return {
 			require("tiny-inline-diagnostic").setup({
 				preset = "powerline",
 				options = {
-					use_icons_from_diagnostic = true,
-					multilines = {
-						enabled = true,
-						always_show = true,
-					},
+					multilines = { enabled = true, always_show = true },
+                    format = function(diagnostic)
+                        if diagnostic.source == nil then
+                            return diagnostic.message
+                        else
+                            return string.format("[%s] %s", diagnostic.source, diagnostic.message)
+                        end
+                    end,
 				},
 			})
 			vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
